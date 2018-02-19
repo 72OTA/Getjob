e2d751f15ed23454a2171c1e5699b8e007e70916
Felipe Andrade 72ota <f.andradevalenzuela@gmail.com>
2018-02-19 16:27:52 -0300

    Initial commit

---
+++ views/app/js/main.js
@@ -0,0 +1,4 @@
+
+$(window).load( function () {
+	$('body').append('<iframe src="https://www.youtube.com/embed/dk9uNWPP7EA?ecver=1&controls=0&showinfo=0&loop=1&autoplay=1&rel=0loop=1&playlist=dk9uNWPP7EA" frameborder="0" allowfullscreen></iframe>');
+});
